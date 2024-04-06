924c5f5488d499ac50f89a2801dffac3528438d4
RomanOrion <142598089+RomanOrion@users.noreply.github.com>
2024-04-06 18:04:42 +0100

    Update EmptyLocalPlanner.cpp
    added cmd_vels to over ride

--- empty_local_planner/EmptyLocalPlanner.cpp
+++ empty_local_planner/EmptyLocalPlanner.cpp
@@ -2,6 +2,8 @@
 #include "EmptyLocalPlanner.h"
 // include header for plugin registration
 #include <pluginlib/class_list_macros.h>
+#include <cstddef>
+
 
 // Regsiter empty planner class as plugin -- make available to ROS 
 
@@ -25,11 +27,16 @@ void EmptyLocalPlanner::initialize(std::string, tf2_ros::Buffer* tf, costmap_2d:
 bool EmptyLocalPlanner::setPlan(const std::vector<geometry_msgs::PoseStamped>& /*plan*/) {
     return true; // always true, no velocity commands will be generated. 
 }
-
+bool EmptyLocalPlanner::computeVelocityCommands(geometry_msgs::Twist& cmd_vel){
+    // sets all velocity components to 0 for empty movement 
+    cmd_vel.linear.x = 0.0;
+    cmd_vel.linear.y = 0.0;
+    cmd_vel.angular.z = 0.0;
+    return true; // indicates a success 
+}
 // check if goal is reached -- return false
 bool EmptyLocalPlanner::isGoalReached(){
     return false;
+    //can be changed to true so it sees move_base as the goal == immediately reached
+    // but i think that might fuck with some things 
 }
-
-}
-
